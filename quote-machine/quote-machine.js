1479cffe0215c0c63fdee3177d5e934792ee0554
Long Hung Nguyen <longhungn98@gmail.com>
2017-09-21 00:41:18 +1000

    final version

--- quote-machine/quote-machine.js
+++ quote-machine/quote-machine.js
@@ -1,23 +1,22 @@
 $(document).ready(function() {
-
+    getNewQuote();
     $("#get-new-quote").click(getNewQuote);
+
+    $("#share-tweet").click(function() {
+        var link = "https://twitter.com/intent/tweet?text="
+        link += $("#quote-content").text() + " - " +$("#author").text();
+        window.open(link);
+    });
 });
 
 function getNewQuote() {
-    // $.getJSON( "https://quotesondesign.com/wp-json/posts?filter%5Borderby%5D=rand&filter%5Bposts_per_page%5D=1&callback=", function(data) {
-    //     var post = data[0]; //data is array with 1 object
-    //     var quote = post.content;
-    //     var author = post.title;
-    //     $("#quote-content").html(quote);
-    //     $("#author").html(author);
-    // });
     $.ajax({
-           url: "https://quotesondesign.com/wp-json/posts?filter%5Borderby%5D=rand&filter%5Bposts_per_page%5D=1&callback=",
-           success: function (data) {
-               var post = data.shift();
-               $("#quote-content").html(post.content);
-               $("#author").html("<em> " + post.title + "</em>");
-           },
-           cache: false
-       });
+        url: "https://quotesondesign.com/wp-json/posts?filter%5Borderby%5D=rand&filter%5Bposts_per_page%5D=1&callback=",
+        success: function(data) {
+            var post = data.shift();
+            $("#quote-content").html(post.content);
+            $("#author").html("<em> " + post.title + "</em>");
+        },
+        cache: false
+    });
 }
