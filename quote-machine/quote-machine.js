aaf23bcbad6ed2fcb55e45b03a8b148e6b615333
Long Hung Nguyen <longhungn98@gmail.com>
2017-09-20 23:13:18 +1000

    try diff js

--- quote-machine/quote-machine.js
+++ quote-machine/quote-machine.js
@@ -15,7 +15,6 @@ function getNewQuote() {
            url: "https://quotesondesign.com/wp-json/posts?filter%5Borderby%5D=rand&filter%5Bposts_per_page%5D=1&callback=",
            success: function (data) {
                var post = data.shift();
-
                $("#quote-content").html(post.content);
                $("#author").html("<em> " + post.title + "</em>");
            },
